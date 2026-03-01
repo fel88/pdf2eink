f7c0b1fe0099c6312a22676f02c7dbb45b10e035
fel88 <15663687+fel88@users.noreply.github.com>
2026-03-01 21:29:26 +0300

    fix

--- pdf2eink/PictureBoxWithInterpolationMode.cs
+++ pdf2eink/PictureBoxWithInterpolationMode.cs
@@ -1,9 +1,11 @@
 ﻿using System.Drawing.Drawing2D;
+using System.ComponentModel;
 
 namespace pdf2eink
 {
     public class PictureBoxWithInterpolationMode : PictureBox
     {
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
         public InterpolationMode InterpolationMode { get; set; }
 
         protected override void OnPaint(PaintEventArgs paintEventArgs)
